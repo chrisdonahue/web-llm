dd720744ed0df9e32c2e1166c19b1f3a66af474f
Chris Donahue <christopherdonahue@gmail.com>
2023-12-02 00:23:44 +0000

    Initial progress

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,7 @@ export interface ChatConfig {
   local_id: string;
   model_lib: string;
   tokenizer_files: Array<string>;
+  tokenizer_manual: Object
   conv_config?: Partial<ConvTemplateConfig>;
   conv_template: string;
   // additional metadata
@@ -45,17 +46,17 @@ export interface AppConfig {
 /**
  * default libmap used in prebuilt
  */
-export const prebuiltAppConfig : AppConfig = {
-	model_list: [
-		{
-			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-RedPajama-INCITE-Chat-3B-v1-q4f32_0/resolve/main/",
-			"local_id": "RedPajama-INCITE-Chat-3B-v1-q4f32_0"
-		},
-		{
-			"model_url": "https://huggingface.co/mlc-ai/mlc-chat-vicuna-v1-7b-q4f32_0/resolve/main/",
-			"local_id": "vicuna-v1-7b-q4f32_0"
-		}
-	],
+export const prebuiltAppConfig: AppConfig = {
+  model_list: [
+    {
+      "model_url": "https://huggingface.co/mlc-ai/mlc-chat-RedPajama-INCITE-Chat-3B-v1-q4f32_0/resolve/main/",
+      "local_id": "RedPajama-INCITE-Chat-3B-v1-q4f32_0"
+    },
+    {
+      "model_url": "https://huggingface.co/mlc-ai/mlc-chat-vicuna-v1-7b-q4f32_0/resolve/main/",
+      "local_id": "vicuna-v1-7b-q4f32_0"
+    }
+  ],
   model_lib_map: {
     "vicuna-v1-7b-q4f32_0": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/vicuna-v1-7b-q4f32_0-webgpu-v1.wasm",
     "RedPajama-INCITE-Chat-3B-v1-q4f32_0": "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/RedPajama-INCITE-Chat-3B-v1-q4f32_0-webgpu-v1.wasm"
